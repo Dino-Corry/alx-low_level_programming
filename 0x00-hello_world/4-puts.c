43cdaec908de96a747610ccb1c66a4f99f5caf1b
Dino-Corry <essen653@gmail.com>
2022-09-10 15:16:31 -0700

    betty wahala fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main () {
-  puts("\"Programming is like building a multilingual puzzle\n");
-return 0;
+/**
+ * main - Entry point
+ *
+ * Description: 'programe to print the puts output standard'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
