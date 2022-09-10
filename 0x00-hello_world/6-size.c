fd22f542def00f044a3b95f1ec33cec34916fd0c
Dino-Corry <essen653@gmail.com>
2022-09-10 16:33:08 -0700

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@
 
 int main(void)
 {
-printf("Size of a char: %lu bytes\n", sizeof(charType));
-printf("Size of an int: %lu bytes\n", sizeof(intType));
-printf("Size of a long int: %lu bytes\n", sizeof(longIntType));
-printf("Size of a long long int: %lu bytes\n", sizeof(longLongIntType));
-ptintf("Size of a float: %lu bytes\n", sizeof(floatType));
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu bytes\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+ptintf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 return (0);
 }
