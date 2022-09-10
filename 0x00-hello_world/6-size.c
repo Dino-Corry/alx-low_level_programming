659f2fa33271772d40561d52bef7f752072a8c84
Dino-Corry <essen653@gmail.com>
2022-09-10 16:26:05 -0700

    update size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+printf("Size of a char: %lu bytes\n", sizeof(charType));
+printf("Size of an int: %lu bytes\n", sizeof(intType));
+printf("Size of a long int: %lu bytes\n", sizeof(longIntType));
+printf("Size of a long long int: %lu bytes\n", sizeof(longLongIntType));
+ptintf("Size of a float: %lu bytes\n", sizeof(floatType));
+
+return (0);
+}
