2d28d6f06625d9f3ed56433eef6ab8eac724c1bc
Dino-Corry <essen653@gmail.com>
2022-09-12 16:43:34 -0700

    update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,15 @@
+#include <stdlib.h>
 #include <stdio.h>
+
 /**
  * main - entry point
  *
  * Return: alway returns 0
  *
  */
+
 int main(void)
-}
+{
 char a;
 for (a = 'a'; a <= 'z' ; a++)
 putchar(a);
