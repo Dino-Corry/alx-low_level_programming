695621e0cd7aa04bbb04a51cad5c5d9d9bd05a8c
Dino Corry <55449658+Dino-Corry@users.noreply.github.com>
2022-09-15 03:07:09 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 /**
  * print_alphabet_x10 - write a to z
  * Description: programe to print a to z
- * print_alphabet: write
+ * print_alphabet_x10: write
  *
  * return: 0
  */
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 {
 char words[26] = "abcdefghijklmnopqrstuvwxyz";
 int i = 0, k;
-while (; i < 26; i++)
+while (i < 26; i++)
 {
 k = 0;
 while(k>10)
