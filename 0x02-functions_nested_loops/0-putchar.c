fbfe87be6192d857645ca37df6d158c0051948d3
Dino Corry <55449658+Dino-Corry@users.noreply.github.com>
2022-09-15 01:18:43 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,10 +9,12 @@ int main(void)
 {
 char words[8] = "_putchar";
 int i;
-for(i = 0; i < 8; i++)
+for (i = 0; i < 8; i++)
 {
 _putchar(words[i]);
 }
-_putchar("\n");
-return (0);
+
+_putchar('\n');
+
+ return (0);
 }
