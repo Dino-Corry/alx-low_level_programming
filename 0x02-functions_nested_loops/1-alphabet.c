bda2d420908b8ceefa56520e071db5fc50aa1e1f
Dino-Corry <essen653@gmail.com>
2022-09-14 17:59:33 -0700

    update

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Description: Program description
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int n = "print_alphabet";
+for (n = 'a'; n <= 'z'; n++;)
+_putchar(n);
+_putchar('\n');
+return (0);
+}
