ddd8b904cd62c1c51874b30b94a96c319eea85ec
Dino Corry <55449658+Dino-Corry@users.noreply.github.com>
2022-09-17 03:02:48 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_numbers - programe to multiplies
- * @a: An integer a
+ * print_numbers - programe to print
+ * 
  * Description: print numbers
  *
  * Return: Always 0
  */
 
 void print_numbers(void)
+{
 int a;
 for (a = 48; a <= 57; a++)
 {
 putchar(a);
 }
 putchar(10);
+}
