ed7b0945200e1211866ced46d9cc21b08db75af0
Dino Corry <55449658+Dino-Corry@users.noreply.github.com>
2022-09-17 02:28:45 +0100

    Create 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - programe to multiplies
+ * @a: An integer a
+ * Description: Function that adds two integers and returns the result
+ *
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+int a;
+for (a >= 0, a <= 9, a++)
+  printf("%d\n" a)
+}
