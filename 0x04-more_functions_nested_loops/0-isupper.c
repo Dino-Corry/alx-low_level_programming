d4d91f4fbdb603740dd4437ece7cea9e178efc3b
Dino Corry <55449658+Dino-Corry@users.noreply.github.com>
2022-09-17 01:23:10 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,18 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
- char c;
- c = 'A'
- if (c = 'A'; c < 'Z' c++)
- {
- printf("%c: %d\n", c, _isupper(c));
- return (1);
- }
- else
-  return (0);
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
