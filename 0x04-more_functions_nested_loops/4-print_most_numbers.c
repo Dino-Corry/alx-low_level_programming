4e1b590ac016ed9c81103f60bf94217f156671f1
Dino Corry <55449658+Dino-Corry@users.noreply.github.com>
2022-09-17 03:15:51 +0100

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_most_numbers - function that print most numbers
+*
+*Return: returns nothing
+*/
+void print_most_numbers(void)
+{
+  int x;
+  if (x = 48, x <= 57, x++)
+  {
+    _putchar(n);
+  }
+    else if (x == 2 && x == 4)
+    {
+      _putchar(n + 1);
+    }
+  return (0);
+}
