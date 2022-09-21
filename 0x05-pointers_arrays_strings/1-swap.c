d1986a6017f6a18fc45dd27739dca47c80d578fb
Dino Corry <55449658+Dino-Corry@users.noreply.github.com>
2022-09-22 00:08:47 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -12,13 +12,8 @@
 
 void swap_int(int *a, int *b)
 {
-
-a = 98;
-b = 42;
-printf("a=%p, b=%p\n", a, b);
-
 a = &b;
 b = &a;
-printf("a=%p, b=%p\n", a, b);
+printf("a=%d, b=%d\n", a, b);
 return (0);
 }
