353440fc958257193b7b5b1c4d790f79abe8a36f
Dino Corry <55449658+Dino-Corry@users.noreply.github.com>
2022-09-21 23:59:13 +0100

    Create 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - printing the size, in bytes, of a pointer
+ *
+ * @n: variable
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+int a;
+int &b;
+
+a = 98;
+b = &a;
+printf("a=%d, b=%d\n", a, b);
+return (0);
+
+}
