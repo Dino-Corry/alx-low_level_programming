e94aaf5586d7031d35a98fd841acdc8eb885cd4c
Dino Corry <55449658+Dino-Corry@users.noreply.github.com>
2022-09-22 00:54:50 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,19 +2,18 @@
 #include "main.h"
 
 /**
- * _strlen - return the length of string
+ * _puts - return the length of string
  *
- * @s: string to evaluate
- * Return: return the length
+ * @str: string to evaluate
  */
 
 void _puts(char *str)
 {
-	int i;
+int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+for (i = 0; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
