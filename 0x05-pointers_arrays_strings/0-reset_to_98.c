d060d9c7633dd4c39d86658b9ddf48f5707677fa
Dino Corry <55449658+Dino-Corry@users.noreply.github.com>
2022-09-21 23:32:21 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,18 +2,18 @@
 #include "main.h"
 
 /**
- * main - printing the size, in bytes, of a pointer
+ * reset_to_98 - printing the size, in bytes, of a pointer
  *
+ * @n - variable
  * Return: Always 0.
  */
- 
- void reset_to_98(int *n)
- {
- int *n;
- 
- n = 98;
- 
- printf("%p" is the value of n\n", n);
- return (0);
- }
- 
+
+void reset_to_98(int *n)
+{
+int *n;
+
+n = 98;
+
+printf("%p" is the value of n\n", n);
+return (0);
+}
