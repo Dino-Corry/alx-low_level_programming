ecb8b04d4f3014a08a3a8e0d218d8930146766bf
Dino Corry <55449658+Dino-Corry@users.noreply.github.com>
2022-09-22 00:12:18 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,16 +4,14 @@
 /**
  * swap_int - printing the size, in bytes, of a pointer
  *
- * @*a: variable
- * @*b: variable
+ * @*a: first pointer
+ * @*b: second pointer
  *
- * Return: Always 0.
+ * 
  */
 
 void swap_int(int *a, int *b)
 {
 a = &b;
 b = &a;
-printf("a=%d, b=%d\n", a, b);
-return (0);
 }
