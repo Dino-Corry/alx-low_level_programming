07023b11f75ad2bf210fa8658ca1a18e56506666
Dino Corry <55449658+Dino-Corry@users.noreply.github.com>
2022-09-23 00:58:39 +0100

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h)
+
+/**
+* _strlen - return the length of string
+*
+* @s: integer
+* Return: always 0
+*/
+
+int _strlen(char *s)
+char *str = "My first strlen!";
+printf("%d\n", strlen(str));
+return (0);
+}
