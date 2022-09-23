94b50c7d0d2b9e3a07281f47eab736770581d219
Dino Corry <55449658+Dino-Corry@users.noreply.github.com>
2022-09-24 00:07:36 +0100

    Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strlen - return the length of string
+*
+* @s: integer
+* Return: always 0
+*/
+
+void _puts(char *str)
+{
+puts("%c\0", str);
+return (0);
+}
