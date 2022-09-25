6f882c202f3659e85f75b86c41120c5c5751c140
Dino Corry <55449658+Dino-Corry@users.noreply.github.com>
2022-09-25 19:22:55 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,53 +2,14 @@
 #include <stdio.h>
 
 /**
- * print_rev - prints a string in reverse, followed by a new line
+ * rev_string - string in reverse
  * @s: string to be printed
  */
-void rev_string(char *s)
+void rev_srting(char *s)
 {
-int l, i;
-
-char *begin_ptr, *end_ptr, ch;
-
-// Get the length of the string
-
-l = strlen(s);
-
-// Set the begin_ptr and end_ptr
-
-// initially to start of string
-
-begin_ptr = s;
-
-end_ptr = s;
-
-// Move the end_ptr to the last character
-
-for (i = 0; i < l - 1; i++)
-
-end_ptr++;
-
-// Swap the char from start and end
-
-// index using begin_ptr and end_ptr
-
-for (i = 0; i < l / 2; i++) {
-
-// swap character
-
-ch = *end_ptr;
-
-*end_ptr = *begin_ptr;
-
-*begin_ptr = ch;
-
-// update pointers positions
-
-begin_ptr++;
-
-end_ptr--;
-
-}
-
+int i = 0, n, a;
+a = i;
+for (n = a - 1; n >= 0;)
+{
+n--;
 }
