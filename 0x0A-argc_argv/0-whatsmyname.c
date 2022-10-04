1196db18d40de713449040595cfa048de11575b9
Dino Corry <55449658+Dino-Corry@users.noreply.github.com>
2022-10-04 22:09:53 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,36 @@
+#include "main.h"
 #include <stdio.h>
 
-/**
- * main - programme that prints its name, followed by a new line
- *
- * @argc: Argument count
- *
- * @argv: Argument vector
- *
- */
+void print_string(char *str);
 
-int main(int argc, char* argv[])
+/**
+* main - Prints its name
+* @argc: Length of argv
+* @argv: Arrays of strings of the program arguments
+* Return: 0, Success
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
 {
-printf("%s\n", argv);
-return (0);
+	print_string(argv[0]);
+	putchar('\n');
+
+	return (0);
+}
+
+/**
+* print_string - Prints all char of a string
+* @str: Pointer to string
+* Return: void
+*/
+
+void print_string(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		putchar(str[i]);
+		i++;
+	}
 }
