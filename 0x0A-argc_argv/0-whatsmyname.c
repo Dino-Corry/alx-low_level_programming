eed08e201bfaa46ddd647a674ffb56a146883696
Dino-Corry <essen653@gmail.com>
2022-10-04 13:18:46 -0700

    argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - programme that prints its name, followed by a new line
+ *
+ * @argc: Argument count
+ *
+ * @argv: Argument vector
+ *
+ */
+
+int main (int argc, char *argv[])
+{
+  return 0;
+}
