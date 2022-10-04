3d2dc4db6c97201af83e8485f30f8c61e5277420
Dino Corry <55449658+Dino-Corry@users.noreply.github.com>
2022-10-04 22:38:12 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,31 @@
-#include <stdio.h>
 #include "main.h"
 
+void print_number(int num);
+
 /**
-* main - Prints its name
-* @argc: Length of argv
-* @argv: Arrays of strings of the program arguments
+* main - Prints the number of arguments passed into it
+* @argc: Length of @argv
+* @argv: Array of strings of the programs arguments
 * Return: 0, Success
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	putchar("%d", argc);
-	putchar('\n');
-
+	print_number(--argc);
+	_putchar('\n');
 	return (0);
+}
+
+/**
+* print_number - prints number with _putchar
+* @num: inter, number to be printed
+* Return: void
+*/
+
+void print_number(int num)
+{
+	if (num / 10)
+		print_number(num / 10);
+
+	_putchar(num % 10 + '0');
 }
