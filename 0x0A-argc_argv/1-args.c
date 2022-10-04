fd7809c4cc0fa45752a772f0a40747f03e22c7dd
Dino Corry <55449658+Dino-Corry@users.noreply.github.com>
2022-10-04 22:24:44 +0100

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - Prints its name
+* @argc: Length of argv
+* @argv: Arrays of strings of the program arguments
+* Return: 0, Success
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d", argc);
+	putchar('\n');
+
+	return (0);
+}
