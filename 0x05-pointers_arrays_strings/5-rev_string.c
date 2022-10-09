c971503eb299248caa9f79781589923fb9b987c6
Dino Corry <55449658+Dino-Corry@users.noreply.github.com>
2022-10-09 01:49:37 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,18 +1,23 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * rev_string - string in reverse
- * @s: string to be printed
+ * rev_string - reverses a string
+ * @s: string to reverse
  */
+
 void rev_string(char *s)
 {
-int l = strlen(s), m = l / 2;
-char temp;
-for (int i = 0; i < m; i++)
-{
-temp = s[i];
-s[i] = s[i - i - 1];
-s[l - i - 1] = temp;
-}
+	char firstStr;
+	int pointer = 0;
+	int counter;
+
+	while (s[pointer] != '\0')
+		pointer++;
+
+	for (counter = 0; counter < pointer; counter++)
+	{
+		pointer--;
+		firstStr = s[counter];
+		s[counter] = s[pointer];
+		s[pointer] = firstStr;
+	}
 }
