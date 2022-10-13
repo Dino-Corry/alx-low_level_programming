121dd073e2067c227d59db9fceea27ca245f011e
Dino Corry <55449658+Dino-Corry@users.noreply.github.com>
2022-10-13 15:29:16 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,15 +10,13 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *new_array;
-	unsigned int i, length;
-  i = 0;
-  length = n;
+	unsigned int i = 0, length = n;
 
 	if (s1 == NULL)
-		s1 = "\n";
+		s1 = "";
 
 	if (s2 == NULL)
-		s2 = "\n";
+		s2 = "";
 
 	while (s1[i++] != '\0')
 		length++;
